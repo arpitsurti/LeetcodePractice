d4a71484b2818d16090d1e0195c1834c8b9b916a
ARPIT <arpittempacc@gmail.com>
2022-02-13 13:39:51 -0500

    496. Next Greater Element I 1544. Make The String Great 1047. Remove All Adjacent Duplicates In String

---
+++ LeetcodePractice/Easy/clsMakeTheStringGreat.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodePractice.Easy
+{
+    
+    public class clsMakeTheStringGreat
+    {
+        /*1544. Make The String Great
+        Given a string s of lower and upper case English letters.
+        A good string is a string which doesn't have two adjacent characters s[i] and s[i + 1] where:
+        0 <= i <= s.length - 2
+        s[i] is a lower-case letter and s[i + 1] is the same letter but in upper-case or vice-versa.
+        To make the string good, you can choose two adjacent characters that make the string bad and remove them. You can keep doing this until the string becomes good.
+        Return the string after making it good. The answer is guaranteed to be unique under the given constraints.
+        Notice that an empty string is also good.
+
+        Example 1:
+        Input: s = "leEeetcode"
+        Output: "leetcode"
+        Explanation: In the first step, either you choose i = 1 or i = 2, both will result "leEeetcode" to be reduced to "leetcode".
+    
+        Example 2:
+        Input: s = "abBAcC"
+        Output: ""
+        Explanation: We have many possible scenarios, and all lead to the same answer. For example:
+        "abBAcC" --> "aAcC" --> "cC" --> ""
+        "abBAcC" --> "abBA" --> "aA" --> ""
+    
+        Example 3:
+        Input: s = "s"
+        Output: "s"
+
+        Constraints:
+        1 <= s.length <= 100
+        s contains only lower and upper case English letters.
+    */
+        public string MakeGood(string s)
+        {
+            Stack<char> st = new Stack<char>();
+            st.Push(s[s.Length - 1]);
+            for (int i = s.Length - 2; i >= 0; i--)
+            {
+                char peek = st.Count == 0 ? ' ' : st.Peek();
+                if ((char)(s[i] + 32) == peek || s[i] == (char)(peek + 32))
+                    st.Pop();
+                else
+                    st.Push(s[i]);
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < st.Count; i++)
+            {
+                sb.Append(st.Pop());
+            }
+            return sb.ToString();
+        }
+    }
+}
--- LeetcodePractice/Program.cs
+++ LeetcodePractice/Program.cs
@@ -11,8 +11,27 @@ namespace LeetcodePractice
     {
         static void Main(string[] args)
         {
-            clsSplitAStringInBalancedStrings obj = new clsSplitAStringInBalancedStrings();
-            obj.BalancedStringSplit("RLRRLLRLRL");
+
+            NextGreaterElement(new int[] {1,3,5,2,4},new int[]{6,5,4,3,2,1,7});
         }
+
+         public static int[] NextGreaterElement(int[] nums1, int[] nums2) 
+         {
+            Dictionary<int,int> dict = new Dictionary<int,int>();
+            Stack<int> st = new Stack<int>();
+            for(int i=0;i<nums2.Length;i++)
+            {
+                while(st.Count != 0 && st.Peek() < nums2[i])
+                    dict.Add(st.Pop(),nums2[i]);
+                st.Push(nums2[i]);
+            }
+        
+            for(int i=0;i<nums1.Length;i++)
+            {
+                
+                nums1[i] = dict.ContainsKey(nums1[i]) ? dict[nums1[i]] : -1;
+            }
+            return nums1;
+         }
     }
 }
